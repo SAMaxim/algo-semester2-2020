293d06a9e70509b64511c20547f2b55d4cf39048
SAMaxim <64688151+SAMaxim@users.noreply.github.com>
2020-12-20 23:04:02 +0300

    task - E

--- assignment-2/task-5.cxx
+++ assignment-2/task-5.cxx
@@ -1,11 +1,64 @@
-/****************************************************************************
-Ricked out
-****************************************************************************/
-
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 int main()
 {
-    return 0;
-}
+	int N, M, S;
+	float V;
+	std::cin >> N >> M >> S >> V;
+	--S;
 
+	vector<vector<int>>   G(N);
+	vector<vector<float>> C(N);
+	vector<vector<float>> R(N);
+
+	for (int i = 0; i < M; ++i)
+	{
+		int u, v;
+		float Rvu, Cvu, Ruv, Cuv;
+		std::cin >> v >> u >> Rvu >> Cvu >> Ruv >> Cuv;
+		--v;
+		--u;
+		G[v].push_back(u);
+		G[u].push_back(v);
+		R[v].push_back(Rvu);
+		R[u].push_back(Ruv);
+		C[v].push_back(Cvu);
+		C[u].push_back(Cuv);
+	}
+
+	vector<float> V0(N, 0);
+	V0[S] = V;
+
+	for (int i = 0; i < N - 1; ++i)
+		for (int v = 0; v < G.size(); ++v)
+			for (int iu = 0; iu < G[v].size(); ++iu)
+			{
+				int u = G[v][iu];
+				float Rvu = R[v][iu];
+				float Cvu = C[v][iu];
+				float Vu = (V0[v] - Cvu) * Rvu;
+				if (Vu > V0[u])
+					V0[u] = Vu;
+			}
+
+	for (int v = 0; v < G.size(); ++v)
+		for (int iu = 0; iu < G[v].size(); ++iu)
+		{
+			int u = G[v][iu];
+			float Rvu = R[v][iu];
+			float Cvu = C[v][iu];
+			float Vu = (V0[v] - Cvu) * Rvu;
+			if (Vu > V0[u])
+			{
+				std::cout << "YES";
+				return 0;
+
+			}
+		}
+
+	std::cout << "NO";
+	return 0;
+}

3ba172751a69c1cc92e2afe990e1683e987afd6e
SAMaxim <64688151+SAMaxim@users.noreply.github.com>
2021-01-21 20:49:12 +0300

    Task 1-1

--- assignment-1/task-1.cxx
+++ assignment-1/task-1.cxx
@@ -1,37 +1,73 @@
-/****************************************************************************
-За молоком
-
-Леон и Матильда собрались пойти в магазин за молоком. Их хочет поймать Стэнсфилд, поэтому нашим товарищам нужно сделать это как можно быстрее. Каково минимальное количество улиц, по которым пройдёт хотя бы один из ребят (либо Матильда, либо Леон, либо оба вместе)?
-
-Граф связный и неориентированный. Вершины нумеруются с 1.
-
-Ввод: 
-Первая строка содержит количество вершин n (1 ≤ n ≤ 10^5), количество ребер m (0 ≤ m ≤ 10^5) и номера вершин графа leon, matilda, milk, в которых находятся соответственно Леон, Матильда и магазин с молоком.
-Следующие m строк содержат ребра графа. В каждой строке два числа, разделенные пробелом, если между i и j существует ребро. Гарантируется, что в графе нет петель и мультиребер.
-
-Вывод: Одно число – количество ребер.
-
-
-  in       | out
- ----------|-----
- 3 2 1 2 3 |  2
- 1 3       |
- 2 3       |
-
- 
-  in       | out
- ----------|-----
- 3 2 1 2 3 |  2
- 1 3       |
- 1 2       |
- 
-
-****************************************************************************/
-
 #include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+enum color
+{
+	white,
+	gray,
+	black
+};
+
+void BFS(int v, vector <vector <int >>& g, vector <color>& colors, vector<int>& distance)
+{
+	queue <int> q;
+	q.push(v);
+	colors[v] = gray;
+	while (!q.empty())
+	{
+		int f = q.front();
+		q.pop();
+		colors[f] = black;
+		for (int u : g[f])
+		{
+			if (colors[u] == white)
+			{
+				distance[u] = distance[f] + 1;
+				q.push(u);
+				colors[u] = gray;
+
+			}
+		}
+	}
+
+}
 
 int main()
 {
-    return 0;
-}
+	int nv, ne, v1, v2, v3;
+	cin >> nv >> ne >> v1 >> v2 >> v3;
+	int v1norm = v1 - 1, v2norm = v2 - 1, v3norm = v3 - 1;
+	vector <vector <int>> g(nv);
+	vector <color> colors(nv);
+	vector <int> distance(nv);
+	for (int i = 0; i < ne; ++i)
+	{
+		int v, u;
+		cin >> v >> u;
+		int vnorm = v - 1, unorm = u - 1;
+		g[vnorm].push_back(unorm);
+		g[unorm].push_back(vnorm);
 
+	}
+	vector <int> leon(nv, 0);
+	vector <int> mat(nv, 0);
+	vector <int> milk(nv, 0);
+	BFS(v1norm, g, colors, leon);
+	fill(colors.begin(), colors.end(), white);
+	BFS(v2norm, g, colors, mat);
+	fill(colors.begin(), colors.end(), white);
+	BFS(v3norm, g, colors, milk);
+	int min = 200000;
+	for (int i = 0; i < nv; ++i)
+	{
+		int sum = leon[i] + mat[i] + milk[i];
+		if (sum < min)
+			min = sum;
+	}
+	cout << min;
+	return 0;
+
+}
